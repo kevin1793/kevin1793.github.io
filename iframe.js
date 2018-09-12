09072fae89198c45c46d2925201292e5600246bf
Kevin Claveria <kevin1793@gmail.com>
2018-09-12 13:53:49 -0500

    comment

--- iframe.js
+++ iframe.js
@@ -1,10 +1,10 @@
        console.log('hi');
        // Set data to send over here
         var values = {
-            "email": "test" + Math.ceil(Math.random() * 100000) + "@veratad.com", // This field is required to identify existing customers.
-            "first_name": "Thomas",
-            "last_name": "Canfarotta",
-            "dob": "19820621" // This field must match the following format: YYYYMMDD. This field is also required
+            // "email": "test" + Math.ceil(Math.random() * 100000) + "@veratad.com", // This field is required to identify existing customers.
+            // "first_name": "Thomas",
+            // "last_name": "Canfarotta",
+            // "dob": "19820621" // This field must match the following format: YYYYMMDD. This field is also required
         };
 
         // var values = {
