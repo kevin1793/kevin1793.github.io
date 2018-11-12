cb2b15c76063eb6b997854ca94bcfd8de71a1cdb
Kevin Claveria <kevin1793@gmail.com>
2018-11-12 14:16:41 -0600

    not using php..

---
+++ cosibella/navbar.php
@@ -0,0 +1,45 @@
+<div class="back_up" onclick="backTop();">
+
+</div>
+<div class="nav_cont">
+    <div class="navbar1">
+
+    </div>
+    <div class="navbar2">
+        <div class="nav_items">
+            <a href="main.html">
+                <span  class="nav_item">
+                    Home
+                </span>
+            </a>
+            <a href="nails.html">
+                <span class="nav_item">
+                    Nails
+                </span>
+            </a>
+            <a href="spa.html">
+                <span class="nav_item">
+                    Spa
+                </span>
+            </a>
+            <a href="about-us.html">
+                <span class="nav_item">
+                    About Us
+                </span>
+            </a>
+            <a href="gift-cards.html">
+                <span class="nav_item">
+                    Gift Cards
+                </span>
+            </a>
+            <a href="contact-us.html">
+                <span class="nav_item">
+                    Contact Us
+                </span>
+            </a>
+        </div>
+    </div>
+    <div class="navbar3">
+        <!-- <p>Call Us (568) 247 6700</p> -->
+    </div>
+</div>
